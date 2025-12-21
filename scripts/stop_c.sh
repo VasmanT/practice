ebdb1cc6e39cb97b8d52947966a9bfcb1c04c6a8
Тихомиров Василий Анатольевич <vasmanvas@gmail.com>
2025-12-21 23:48:06 +0300

    stop_c.sh доделал

--- scripts/stop_c.sh
+++ scripts/stop_c.sh
@@ -1,15 +1,11 @@
-
-
 #!/bin/bash
-
-#set -e
 
 # Параметры
 CONTAINER_ID="${1:-9ea}"
 
-print_info "2. Останавливаем контейнер $CONTAINER_ID..."
+echo "2. Останавливаем контейнер $CONTAINER_ID..."
     if docker stop "$CONTAINER_ID" &> /dev/null; then
-        print_success "Контейнер остановлен"
+        echo  "Контейнер остановлен"
     else
-        print_warning "Контейнер уже остановлен или не найден"
+        echo  "Контейнер уже остановлен или не найден"
     fi
